04710aa80a686254260b316252bac59edf5d3ce8
Huỳnh Văn Vọng <huynhvanvong2002@gmail.com>
2023-01-07 14:11:31 +0700

    Build swagger

--- src/main/java/com/poly/datn/BackendApplication.java
+++ src/main/java/com/poly/datn/BackendApplication.java
@@ -1,9 +1,21 @@
 package com.poly.datn;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API for project", version = "v1.0.0"), security = { @SecurityRequirement(name = "bearerAuth") })
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class BackendApplication {
 
     public static void main(String[] args) {
