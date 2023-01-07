898ca1c6df69b4e28bfa951cca5f29ad9eaf2f2d
Huỳnh Văn Vọng <huynhvanvong2002@gmail.com>
2023-01-07 16:01:42 +0700

    add log for exception handle

--- src/main/java/com/poly/datn/exception/ExceptionHandle.java
+++ src/main/java/com/poly/datn/exception/ExceptionHandle.java
@@ -1,5 +1,7 @@
 package com.poly.datn.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,9 +9,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 public class ExceptionHandle extends ResponseEntityExceptionHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleUnwantedException(Exception e) {
-        e.printStackTrace();
+        LOGGER.error(e.getMessage());
 
         return ResponseEntity.status(500).body("Server error");
     }
