0cc56a642ec742e87eca37009fcf418446bb4e12
HiCode <74962312+hieuhoang25@users.noreply.github.com>
2023-02-08 23:17:33 +0700

    update code

--- src/main/java/com/poly/datn/controller/router/Router.java
+++ src/main/java/com/poly/datn/controller/router/Router.java
@@ -1,8 +1,14 @@
 package com.poly.datn.controller.router;
 
 public class Router {
-    public static class USER_API{
-        public static final String BASE= "/api/user";
-        public static final String FIND_BY= "/{id}";
+    public static class USER_API {
+        public static final String BASE = "/api/user";
+        public static final String FIND_BY = "/{id}";
+        public static final String PRODUCT_FIND_ID = "/product/{id}";
+    }
+
+    public static class ADMIN_API {
+        public static final String BASE = "/api/admin";
+
     }
 }
--- src/main/java/com/poly/datn/service/serviceImpl/UserFindAllImpl.java
+++ src/main/java/com/poly/datn/service/serviceImpl/UserFindAllImpl.java
@@ -8,13 +8,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 @RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
 @Service
 public class UserFindAllImpl implements UserFindAll {
     private final UserRepository userRepository;
+
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
 }
