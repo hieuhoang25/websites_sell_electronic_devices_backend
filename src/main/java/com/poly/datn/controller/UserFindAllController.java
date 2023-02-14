f57309f138cd8ce90ba2b0ff2cff677f65b40d44
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-02-14 23:08:41 +0700

    update: UserFindAllController

--- src/main/java/com/poly/datn/controller/UserFindAllController.java
+++ src/main/java/com/poly/datn/controller/UserFindAllController.java
@@ -1,10 +1,13 @@
 package com.poly.datn.controller;
 
+import com.poly.datn.common.MessageResponse;
+import com.poly.datn.common.ResponseBody;
 import com.poly.datn.common.mapper.ModelConverter;
 import com.poly.datn.controller.router.Router;
 import com.poly.datn.dto.response.UserFindAllResponse;
 import com.poly.datn.service.UserFindAll;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +22,13 @@ public class UserFindAllController {
     private final ModelConverter modelConverter;
 
     @GetMapping
-    public List<UserFindAllResponse> userFindAll() {
-        return modelConverter.mapAllByIterator(userFindAll.findAll(), UserFindAllResponse.class);
+    public ResponseEntity<ResponseBody<List<UserFindAllResponse>>> userFindAll() {
+        return ResponseEntity.ok(
+                new ResponseBody<>(
+                        1,
+                        MessageResponse.MESSAGE_SUCCESS,
+                        modelConverter.mapAllByIterator
+                                (userFindAll.findAll(), UserFindAllResponse.class))
+        );
     }
 }
