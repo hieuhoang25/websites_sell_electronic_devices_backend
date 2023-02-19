f3b908597f14bd310deb35aa0b69e9c0ffd02e25
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-02-19 23:27:10 +0700

    do: find specific productVariant

--- src/main/java/com/poly/datn/repository/ProductVariantRepository.java
+++ src/main/java/com/poly/datn/repository/ProductVariantRepository.java
@@ -2,6 +2,10 @@ package com.poly.datn.repository;
 
 import com.poly.datn.entity.ProductVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductVariantRepository extends JpaRepository<ProductVariant, Integer> {
+    @Query("select o from ProductVariant o where o.product.id=:productId " +
+            "and o.color.id=:colorId and o.status=true and o.storage.id=:storageId")
+    ProductVariant findByProductAndColor(Integer colorId, Integer productId, Integer storageId);
 }
