926e777aa7bdc7af5d45e4e6513cbb8358822fc3
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-02-25 15:40:44 +0700

    do: init OrderStatusResponse

---
+++ src/main/java/com/poly/datn/dto/response/OrderStatusResponse.java
@@ -0,0 +1,13 @@
+package com.poly.datn.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStatusResponse {
+    private Integer id;
+    private String name;
+}
