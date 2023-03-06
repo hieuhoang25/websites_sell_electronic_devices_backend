240a3ddbd9679bdb0629d1a22441f937b677eeea
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-03-06 23:19:56 +0700

    do: move to config package

--- src/main/java/com/poly/datn/config/WebMvcConfig.java
+++ src/main/java/com/poly/datn/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.poly.datn.security;
+package com.poly.datn.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
