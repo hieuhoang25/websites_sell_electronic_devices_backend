b33743ded8c1bc15e486d8a773e80f3771c8a546
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-03-06 12:53:49 +0700

    do: init LoginRequest

---
+++ src/main/java/com/poly/datn/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.poly.datn.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+    @NotBlank
+    @NotNull
+    private String userName;
+    @NotBlank
+    @NotNull
+    private String password;
+}
