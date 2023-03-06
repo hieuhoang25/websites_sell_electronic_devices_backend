4c7aa9ef503a586ea5203ad7699cbcccf520f3c1
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-03-06 13:15:09 +0700

    update security

--- src/main/java/com/poly/datn/security/WebSecurityConfig.java
+++ src/main/java/com/poly/datn/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -55,4 +56,8 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/swagger-ui/**","/v3/api-docs/**");
+    }
 }
