d835881b07bc7a8cd87ac71d250dda38a013ec59
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-03-11 22:58:39 +0700

    do: custom findAll

--- src/main/java/com/poly/datn/repository/CategoryRepository.java
+++ src/main/java/com/poly/datn/repository/CategoryRepository.java
@@ -2,6 +2,11 @@ package com.poly.datn.repository;
 
 import com.poly.datn.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    @Query("select o from Category o where o.parent is null")
+    List<Category> findAll();
 }
