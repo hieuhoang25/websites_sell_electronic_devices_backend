7acd97fb0d63470d431d6b84f5e1cd9252213873
vantht <hoangthuyvantran@gmail.com>
2023-03-12 18:28:58 +0700

    create current user method
    in UserInfoByTokenService

--- src/main/java/com/poly/datn/service/UserInfoByTokenService.java
+++ src/main/java/com/poly/datn/service/UserInfoByTokenService.java
@@ -1,7 +1,11 @@
 package com.poly.datn.service;
 
 import com.poly.datn.dto.response.UserLoggedResponse;
+import com.poly.datn.entity.User;
 
 public interface UserInfoByTokenService {
     UserLoggedResponse getUserInfo();
+
+    // get user entity of current logged-in user
+    User getCurrentUser();
 }
