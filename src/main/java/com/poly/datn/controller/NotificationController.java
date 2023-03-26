75ebd50b35b8b25f8cf1f6df08792ca9f60b61b1
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-03-27 00:28:53 +0700

    clean and format code

--- src/main/java/com/poly/datn/controller/NotificationController.java
+++ src/main/java/com/poly/datn/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package com.poly.datn.controller;
 
 import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class NotificationController {
-    @Autowired
-    private SimpMessageSendingOperations messagingTemplate;
+    private final SimpMessageSendingOperations messagingTemplate;
 
     // use for check server live or die
     @MessageExceptionHandler
