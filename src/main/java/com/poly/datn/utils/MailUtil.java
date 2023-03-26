6e082f73f9121ec577a69e0a01273371ba2d86e4
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-03-27 00:28:24 +0700

    add valid email method

--- src/main/java/com/poly/datn/utils/MailUtil.java
+++ src/main/java/com/poly/datn/utils/MailUtil.java
@@ -94,4 +94,10 @@ public class MailUtil {
     public void sendPromotionForUser(){
         //...
     }
+
+    public static Boolean validateEmail(String email){
+       String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+               + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+       return email.matches(regexPattern);
+    }
 }
