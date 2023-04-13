6917ce8f52cffa5056e976ea5c6cecc6756e5aed
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-04-13 15:55:31 +0700

    add @valid

--- src/main/java/com/poly/datn/controller/RegisterController.java
+++ src/main/java/com/poly/datn/controller/RegisterController.java
@@ -5,32 +5,38 @@ import com.poly.datn.dto.request.RegisterRequest;
 import com.poly.datn.service.RegisterService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 import static com.poly.datn.controller.router.Router.API.*;
 
 @RestController
-@RequestMapping(Router.API.BASE )
+@RequestMapping(Router.API.BASE)
 @AllArgsConstructor
 @Tag(name = Router.API.BASE + REGISTER)
+@Validated
 public class RegisterController {
 
     private RegisterService registerService;
 
     @PostMapping(REGISTER)
-    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest){
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest registerRequest) {
         return ResponseEntity.ok(registerService.register(registerRequest));
     }
 
     @PostMapping(RESEND_OTP)
-    public ResponseEntity<?> resend(@PathVariable("username") String email){
+    public ResponseEntity<?> resend(@PathVariable("username") @Valid @Email @NotBlank String email) {
         return ResponseEntity.ok(registerService.reSendOTP(email));
     }
 
     @PostMapping(VERIFICATION_OTP)
-    public ResponseEntity<?> verificationOTP(@PathVariable("username") String email, @PathVariable("code") String code){
+    public ResponseEntity<?> verificationOTP(@PathVariable("username") @Valid @Email @NotBlank String email,
+                                             @PathVariable("code") @Valid @NotBlank String code) {
         return ResponseEntity.ok(registerService.verifyOtp(code, email));
     }
 
