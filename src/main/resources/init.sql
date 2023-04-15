d8a35ace4bc270abdda25fb987cdf7e5cde0ec86
vonghuynh <huynhvanvong2002@gmail.com>
2023-04-15 22:40:00 +0700

    change inti.sql and volumn docker

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `account`
 --
-USE database electronic_db;
+USE electronic_db;
 DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
