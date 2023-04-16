369b02fe05fc291c4ffdb13e9ceb43fa30a97db7
vantht <hoangthuyvantran@gmail.com>
2023-04-17 00:10:21 +0700

    fix flashdeal

--- src/main/java/com/poly/datn/controller/FlashDealController.java
+++ src/main/java/com/poly/datn/controller/FlashDealController.java
@@ -5,14 +5,8 @@ import static com.poly.datn.controller.router.Router.FLASH_DEAL.CURRENT;
 import static com.poly.datn.controller.router.Router.FLASH_DEAL.FLASH_DEAL;
 import static com.poly.datn.controller.router.Router.FLASH_DEAL.TODAY;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -25,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.poly.datn.dto.request.FlashDealRequest;
 import com.poly.datn.dto.response.FlashDealResponse;
-import com.poly.datn.entity.PromotionProduct;
 import com.poly.datn.repository.PromotionProductRepository;
 import com.poly.datn.service.FlashDealService;
 
@@ -35,7 +28,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(BASE + FLASH_DEAL)
-@Slf4j
 public class FlashDealController {
 
     final PromotionProductRepository repository;
@@ -44,24 +36,13 @@ public class FlashDealController {
     @GetMapping(TODAY)
     public ResponseEntity<?> getTodayPromoList() {
         try {
-            LocalDateTime start = LocalDate.now().atStartOfDay();
-            LocalDateTime end = LocalDate.now().atTime(LocalTime.MAX);
-            ZoneOffset offset = OffsetDateTime.now().getOffset();
-
-            List<PromotionProduct> list = repository.findByUpdatedDateBetweenAndActivate(start.toInstant(offset),
-                    end.toInstant(offset), true);
-            log.info("size: " + list.size());
-            List<FlashDealResponse> deals = dealService.getTodayFlashDeal();
-
-            if (deals == null)
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong, check log");
+            List<FlashDealResponse> deals = dealService.getTodayFlashDealExcludedExpire();
+            if (deals == null) throw new RuntimeException();          
             return ResponseEntity.ok().body(deals);
         } catch (Exception e) {
             e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong, check log");
         }
-
-        // log.info("product: " list.size());
-        return ResponseEntity.ok("ok");
     }
 
     @GetMapping(CURRENT)
@@ -76,13 +57,9 @@ public class FlashDealController {
 
     @PostMapping
     public ResponseEntity<?> getDealFrom(@Valid @RequestBody FlashDealRequest request) {
-        log.info("start: " + request.getStart().toString());
-        log.info("end: " + request.getEnd().toString());
         try {
-
             List<FlashDealResponse> list = dealService.getFlashDealFrom(request.getStart(), request.getEnd());
-            if (list == null)
-                throw new RuntimeException("Something went wrong!!!");
+            if (list == null) throw new RuntimeException("Something went wrong!!!");
             return ResponseEntity.ok(list);
         } catch (Exception e) {
             e.printStackTrace();
