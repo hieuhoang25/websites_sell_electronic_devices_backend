98ae1d59e8942507b243b3f765068952e12c2ece
vantht <hoangthuyvantran@gmail.com>
2023-04-25 18:48:45 +0700

    Update CartDetail.java

--- src/main/java/com/poly/datn/entity/CartDetail.java
+++ src/main/java/com/poly/datn/entity/CartDetail.java
@@ -84,6 +84,7 @@ public class CartDetail {
         return null;
     }
 
+   
     public Double getPrice_Detail() {
         return price_detail;
     }
@@ -128,4 +129,57 @@ public class CartDetail {
         this.productVariant = productVariant;
     }
 
+    public void setPriceDetails(Double price_detail) {
+        this.price_detail = price_detail;
+    }
+    public void setDiscountAmounts(Double discount_amount) {
+        this.discount_amount = discount_amount;
+    }
+
+    public CartDetail clone() {
+        CartDetail cd = new CartDetail();
+        cd.setCart(this.getCart());
+        cd.setId(this.getId());
+        cd.setQuantity(this.getQuantity());
+        cd.setCreateDate(this.getCreateDate());
+        cd.setProductVariant(this.getProductVariant());
+        cd.setPriceDetails(this.getPrice_Detail());
+        cd.setDiscountAmounts(this.getDiscount_Amount());
+        return cd;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((productVariant == null) ? 0 : productVariant.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CartDetail other = (CartDetail) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (productVariant == null) {
+            if (other.productVariant != null)
+                return false;
+        } else if (!productVariant.equals(other.productVariant))
+            return false;
+        return true;
+    }
+
+    
 }
